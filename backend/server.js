471d8b2997ba67e8049e5d528ceaf10ace4f248a
Victor Seleimend <sseleimend@gmail.com>
2025-10-04 12:29:24 -0300

    feat: add MongoDB connection setup in db.js and integrate with server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -13,4 +14,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
+  connectDB();
 });
