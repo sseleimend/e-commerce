cc8998e26c07a4b02e684f0e5cb85160da650dce
Victor Seleimend <sseleimend@gmail.com>
2025-10-04 14:47:36 -0300

    feat: add cookie-parser middleware to server setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
@@ -9,6 +11,9 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
