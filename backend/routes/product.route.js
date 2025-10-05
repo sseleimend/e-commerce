5408a860ecb8fabd5cba03c93a69c6192486346c
Victor Seleimend <sseleimend@gmail.com>
2025-10-05 12:37:09 -0300

    feat: implement product routes for CRUD operations and recommendations

---
+++ backend/routes/product.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getFeaturedProducts,
+  getRecommendations,
+} from "../controllers/product.controller.js";
+import { adminRoute, protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.get("/recommendations", getRecommendations);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+
+export default router;
