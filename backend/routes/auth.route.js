59637a2e6c8bbee2bf45527e0d9ef3b2cda1a3ce
Victor Seleimend <sseleimend@gmail.com>
2025-10-05 12:36:54 -0300

    feat: protect profile route with authentication middleware

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,6 +6,7 @@ import {
   refreshToken,
   profile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -13,6 +14,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-router.get("/profile", profile);
+router.get("/profile", protectRoute, profile);
 
 export default router;
