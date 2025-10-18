d63060a16384a0e982d0535ef1b00045ec4eb704
Victor Seleimend <sseleimend@gmail.com>
2025-10-17 23:45:16 -0300

    feat: add production build setup in server and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import cookieParser from "cookie-parser";
+
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -18,6 +19,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
@@ -27,6 +30,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/.*/, (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
+  );
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
